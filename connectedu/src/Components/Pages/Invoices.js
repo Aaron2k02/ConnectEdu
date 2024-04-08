01e44460f09984927e8b9bc627462c13ae0f2e0e
Aaron Chee <u2102810@siswa.um.edu.my>
2024-04-08 23:21:34 +0800

    Changes on Project structure

---
+++ connectedu/src/Components/Pages/Invoices.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Invoices() {
+  return (
+      <div>
+          <h1>
+              Notification Page
+          </h1>
+    </div>
+  )
+}
+
+export default Invoices
