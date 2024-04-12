177922143f2ef13a9475ab53a0f22a3126b46778
maesrah <maessrah@gmail.com>
2024-04-12 14:21:15 +0800

    forgotpass

--- connectedu/src/pages/forgotPass/ForgotPass.jsx
+++ connectedu/src/pages/forgotPass/ForgotPass.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./ForgotPass.scss"
 
 const ForgotPass = () => {
   return (
-    <div>
+    <div className='forgotPass'>
+      <div className='container'>
+        <div className='left'>
+        <form action="">
+        <h1>Forgot Password?</h1>
+        <div className='input-box'>
+          <input type="email" placeholder='Email address' required />
+        </div>
+        <button>Submit</button>
+      </form>
+        </div>
+      
+      </div>
+      
       
     </div>
   )
