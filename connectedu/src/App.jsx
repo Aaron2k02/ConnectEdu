676972a314c08e450af9168a8e06624a8c2c26d5
Qhalish <qhalishharoma@gmail.com>
2024-04-20 22:33:59 +0800

    edit import

--- connectedu/src/App.jsx
+++ connectedu/src/App.jsx
@@ -35,7 +35,6 @@ import ManageClass from "./pages/ManageClass/ManageClass";
 import ManageUser from "./pages/ManageUser/ManageUser";
 import CreateCoursePreview from "./pages/createCourseReview/CreateCoursePreview";
 import CreateCourseContent from "./pages/createCourseContent/CreateCourseContent";
-import CreateCoursePreview from "./pages/createCourseReview/CreateCoursePreview";
 import ManageCourses from "./pages/manageCourses/ManageCourses";
 import ViewCourse from "./pages/viewCourse/viewCourse";
 
@@ -135,7 +134,20 @@ function App() {
         {
           path: '/viewCourse',
           element: <ViewCourse />
+        },
+        {
+          path: '/Dashboard',
+          element: <Dashboard />
+        },
+        {
+          path: '/ManageUser',
+          element: <ManageUser/>
+        },
+        {
+          path: '/ManageClass',
+          element: <ManageClass/>
         }
+
       ]
     },
   ]);
