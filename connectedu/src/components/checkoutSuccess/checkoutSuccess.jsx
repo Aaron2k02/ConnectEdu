84c5795832c54d336c46272ee07dd1ebbf06cd59
muqriqawiem <muhammadmuqri32@gmail.com>
2024-04-21 21:20:45 +0800

    Refactored CheckoutSuccess component with styled components and removed scss file

--- connectedu/src/components/checkoutSuccess/checkoutSuccess.jsx
+++ connectedu/src/components/checkoutSuccess/checkoutSuccess.jsx
@@ -1,14 +1,26 @@
-import "./checkoutSuccess.scss"
+import styled from "styled-components";
 
 const CheckoutSuccess = () => {
   return (
-    <div className="card">
-      <div className="checkmark">✓</div>
-      <h1>Payment Successful!</h1>
-      <p>We received your payment. <br /> Your purchased course are available <a href="/myCourses">here</a>!</p>
-    </div>
-
+    <Container>
+            <h2>Payment Successful</h2>
+            <p>Your purchased course available <a href="/myCourses">here</a> !</p>
+        </Container>
   );
 };
-
 export default CheckoutSuccess;
+
+const Container = styled.div`
+  min-height: 50vh;
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h2 {
+    margin-bottom: 0.5rem;
+    color: #388e3c;
+  }
+`;
